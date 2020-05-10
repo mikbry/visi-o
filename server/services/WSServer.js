a93beee9144ce0a073290e58db10f7efd002a27d
Mik <mik@opla.ai>
2020-05-10 20:47:33 +0200

    fix: test websocket

--- server/services/WSServer.js
+++ server/services/WSServer.js
@@ -10,40 +10,32 @@ export class WSServer {
     this.wss = [];
   }
 
-  async handleSocket(sock, request) {
+  async handleSocket(sock, request, ws) {
     try {
-      let ws = this.wss.find(w => w.url === request.url || w.url === '*');
-      console.log('request.path', request.path, request.url, ws);
-      if (ws) {
-        console.log("socket connected! ", request.url);
-        ws.send = async message => sock.send(message);
-        const context = await ws.handler('connection', null, ws);
-        context.ws = ws;
-        try {
-          for await (let ev of sock) {
-            if (isWebSocketPingEvent(ev)) {
-              const [, body] = ev;
-              // ping
-              console.log("ws:Ping", body);
-              await ws.handler('ping',context, ev);
-            } else if (isWebSocketCloseEvent(ev)) {
-              // close
-              const { code, reason } = ev;
-              console.log("ws:Close", code, reason);
-              await ws.handler('close',context, ev);
-            } else {
-              await ws.handler('message', context, ev);
-            }
-          }
-          console.log('closed sock');
-        } catch (err) {
-          console.error(`failed to receive frame: ${err}`);
-  
-          if (!sock.isClosed) {
-            await sock.close(1000).catch(console.error);
+      console.log("socket connected! ", request.url);
+      ws.send = async message => sock.send(message);
+      const context = await ws.handler('connection', null, ws);
+      context.ws = ws;
+      try {
+        for await (const ev of sock) {
+          if (isWebSocketPingEvent(ev)) {
+            const [, body] = ev;
+            // ping
+            console.log("ws:Ping", body);
+            await ws.handler('ping', context, ev);
+          } else if (isWebSocketCloseEvent(ev)) {
+            // close
+            const { code, reason } = ev;
+            console.log("ws:Close", code, reason);
+            await ws.handler('close', context, ev);
+          } else {
+            await ws.handler('message', context, ev);
           }
         }
-      } else {
+        console.log('closed sock');
+      } catch (err) {
+        console.error(`failed to receive frame: ${err}`);
+
         if (!sock.isClosed) {
           await sock.close(1000).catch(console.error);
         }
@@ -56,16 +48,22 @@ export class WSServer {
 
   async listen({ port }) {
     const server = serve(`:${port}`);
-    for await (let request of server) {
-      console.log("new request ", request.url);
-      const { conn, r: bufReader, w: bufWriter, headers } = request;
-      const sock = await acceptWebSocket({
-        conn,
-        bufReader,
-        bufWriter,
-        headers,
-      });
-      await this.handleSocket(sock, request);
+    for await (const request of server) {
+      let ws = this.wss.find(w => w.url === request.url || w.url === '*');
+      console.log('request.path', request.path, request.url, ws);
+      if (ws) {
+        (async () => {
+          console.log("new request ", request.url);
+          const { conn, r: bufReader, w: bufWriter, headers } = request;
+          const sock = await acceptWebSocket({
+            conn,
+            bufReader,
+            bufWriter,
+            headers,
+          });
+          await this.handleSocket(sock, request, ws);  
+        })();
+      }
     }
   }
 
