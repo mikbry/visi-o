781c77df266bc864faac6048eac0a8a13ce96b46
Mik <mik@opla.ai>
2020-05-18 17:33:11 +0200

    feat: design home / lobby #10

--- client/index.js
+++ client/index.js
@@ -27,12 +27,12 @@ const Lobby = () => {
     console.log('todo join room');
   }
   return html`
-  <div>
-    <li>
+  <div class="rooms-box">
+    <ul>
       ${data.rooms.map(room =>html`
-        <ul id=${room.id}>${room.name}</ul>
+        <li id=${room.id}>${room.name}</li>
       `)}
-    </li>
+    </ul>
     <button onClick=${e => handleJoinRoom()}>Join Room</button>
   </div>
   `;
@@ -52,9 +52,16 @@ const VideoRoom = () => {
 ReactDOM.render(
   html`
   <div>
-    <h1>Dino-O</h1>
-    <p>Experimental Video-conversation app using Deno</p>
-    <${Lobby} />
+    <div class="header-container">
+      <div>
+        <div>
+          <h1>Din-O</h1>
+          <p>Experimental Video-conversation app</p>
+        </div>
+        <${Lobby} />
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#bca9a9" fill-opacity="1" d="M0,256L130.9,160L261.8,64L392.7,128L523.6,96L654.5,320L785.5,256L916.4,160L1047.3,288L1178.2,0L1309.1,128L1440,0L1440,320L1309.1,320L1178.2,320L1047.3,320L916.4,320L785.5,320L654.5,320L523.6,320L392.7,320L261.8,320L130.9,320L0,320Z"></path></svg>
+      </div>
   </div>
 `,
   document.body
